5cba1c32e1846fb30a772a8b471c459843787519
Nataliia Melnyk <goodlucknat97@gmail.com>
2022-08-08 18:17:05 +0300

    SQL request methods that return String, Map, List

--- src/main/java/database/DataBase.java
+++ src/main/java/database/DataBase.java
@@ -3,16 +3,13 @@ package database;
 import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStream;
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.SQLException;
-import java.sql.Statement;
-import java.util.Properties;
+import java.sql.*;
+import java.util.*;
 
 public class DataBase {
     Connection con;
-    Statement stmt;
 
+    // Database connection method
     public void connection() throws ClassNotFoundException {
         try {
             InputStream input = new FileInputStream("src\\main\\java\\database\\DBConfig.properties");
@@ -33,13 +30,63 @@ public class DataBase {
         }
     }
 
-    public Statement getStatement() {
-        try {
-            stmt = con.createStatement();
-        } catch (SQLException e) {
-            e.printStackTrace();
+    // SQL request method that return String
+    public String getManager() throws ClassNotFoundException, SQLException {
+        String sql = "select concat(manager.first_name, ' ', manager.last_name) as full_name, phone, email from manager where phone is not null and email like '%@gmail.com'";
+
+        this.connection();
+
+        Statement statement = con.createStatement();
+        ResultSet resultSet = statement.executeQuery(sql);
+
+        StringBuilder managersFullName = new StringBuilder();
+
+        while (resultSet.next()) {
+            String manager = resultSet.getString("full_name");
+            String phone = resultSet.getString("phone");
+            String email = resultSet.getString("email");
+
+            managersFullName.append(manager).append(" ").append(phone).append(" ").append(email);
         }
-        return stmt;
+
+        return managersFullName.toString();
+    }
+
+    // SQL request method that return Map
+    public Map<String, String> getFullName() throws ClassNotFoundException, SQLException {
+        Map<String, String> map = new HashMap<>();
+        String sql = "select concat(manager.first_name, ' ', manager.last_name) as full_name, department.name from manager join department on manager.department_id = department.id where department.name = 'Комната добра'";
+
+        this.connection();
+
+        Statement statement = con.createStatement();
+        ResultSet resultSet = statement.executeQuery(sql);
+
+        while (resultSet.next()) map.put(resultSet.getString("name"), resultSet.getString("full_name"));
+
+        return map;
+    }
+
+    // SQL request method that return List
+    public List<String> getId() throws ClassNotFoundException, SQLException {
+        List<String> financeId = new ArrayList<>();
+
+        String sql = "select ticket.id, category.name from ticket join category on ticket.category_id = category.id and ticket.done_deadline between '19.04.2018' and '03.05.2018' and category.name = 'Финансы'";
+
+        this.connection();
+
+        Statement statement = con.createStatement();
+        ResultSet resultSet = statement.executeQuery(sql);
+
+        while (resultSet.next()) {
+            String id = resultSet.getString("id");
+            String category = resultSet.getString("name");
+
+            financeId.add(id);
+            financeId.add(category);
+        }
+
+        return financeId;
     }
 }
 
--- src/test/java/tests/Task18.java
+++ src/test/java/tests/Task18.java
@@ -3,16 +3,29 @@ package tests;
 import database.DataBase;
 import org.testng.annotations.Test;
 
+import java.sql.SQLException;
+
 public class Task18 {
-    protected static DataBase conn = new DataBase();
+    protected static DataBase managers = new DataBase();
 
     @Test
-    public void db_connection() {
-        try {
-            conn.connection();
+    public static void selectManagersWithContactData() throws SQLException, ClassNotFoundException {
 
-        } catch (ClassNotFoundException e) {
-            e.printStackTrace();
-        }
+        // Select the Full name from the table on Managers page where phone is not empty and email contains @gmail.com
+        System.out.println(managers.getManager());
+    }
+
+    @Test
+    public static void selectManagerWithDepartmentName() throws SQLException, ClassNotFoundException {
+
+        // Select the Full name from the table on Managers page where Department = "Комната добра"
+        System.out.println(managers.getFullName().values());
+    }
+
+    @Test
+    public static void selectIdOfFinanceCategory() throws SQLException, ClassNotFoundException {
+
+        // Select the ID from the table on dashboard page Done Deadline tab where Created from 19.04.2018 to 03.05.2018 and Category = "Финансы"
+        System.out.println(managers.getId());
     }
 }
